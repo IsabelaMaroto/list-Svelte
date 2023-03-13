d39ac53908e823f5f414221170ac93ca973ee6f0
IsabelaMaroto <belamaroto@gmail.com>
2023-03-13 18:16:35 -0300

    update style

--- src/lista-compras/ListaCompras.svelte
+++ src/lista-compras/ListaCompras.svelte
@@ -7,7 +7,7 @@
     $: {
         localStorage.setItem("lista-compras", JSON.stringify(itens));
     }
-    
+
     $: itensPendentes = itens.filter(item => !item.comprado).length;
     let inputEL = null;
 
@@ -26,23 +26,75 @@
     };
 </script>
 
-<h2>Lista de compras</h2>
-<form on:submit|preventDefault={adicionarItem}>
-    <input bind:this={inputEL}/>
-    <button type="submit">Adicionar item</button>
-</form>
+<main>
+    <h2>Lista de compras</h2>
+    <form on:submit|preventDefault={adicionarItem}>
+        <input bind:this={inputEL} placeholder="digite o seu item aqui"/>
+        <button type="submit">Adicionar item</button>
+    </form>
+    
+    <div class="lista">
+        {#if itens.length === 0}
+            <div>A lista está vazia.</div>
+        {:else}
+        {#each itens as item, i}
+            <ListaItem 
+            bind:comprado={item.comprado} 
+            descricao={item.descricao}
+            on:itemremovido = {() => removerItem(item)}
+            />
+        {/each} 
+        {/if}
+    </div>
+
+    <div class="contador">Itens pendentes: {itensPendentes}</div>
+    
+</main>
 
 
-{#if itens.length === 0}
-    <div>A lista está vazia.</div>
-{:else}
-{#each itens as item, i}
-    <ListaItem 
-    bind:comprado={item.comprado} 
-    descricao={item.descricao}
-    on:itemremovido = {() => removerItem(item)}
-    />
-{/each} 
-{/if}
-
-<div>Itens pendentes: {itensPendentes}</div>
+<style>
+    main{
+        background-color:darkseagreen;
+        padding: 2rem 0;
+    }
+    h2{
+        text-align: center;
+        font-size: 2rem ;
+        margin-bottom: 1rem;
+    }
+    form{
+        display: flex;
+        justify-content: center;
+        padding: 1rem;
+        margin-bottom: 1rem;
+    }
+    input{
+        width: 80%;
+        padding: 0.5rem;
+        background-color: hsl(60, 56%, 91%);
+        border: none;
+        margin-right: 0.3rem;
+    }
+    button{
+        padding: 0.5rem;
+        cursor: pointer;
+        border: none;
+        background-color: hsl(60, 56%, 91%);
+        color: hsl(0, 0%, 50%);
+    }
+    button:hover{
+        background-color: hsl(120, 15%, 42%);
+        color: hsl(0, 0%, 100%);
+    }
+    .lista{
+        padding: 1rem 0;
+        width: 98%;
+        margin: 0 auto;
+    }
+    .contador{
+        text-align:center;
+        font-size: 1.2rem;
+        font-weight: bold;
+        color: hsl(0, 1%, 20%);
+    }
+</style>
--- src/lista-compras/ListaItem.svelte
+++ src/lista-compras/ListaItem.svelte
@@ -8,10 +8,36 @@
 </script>
 
 
-<div>
+<div class="container-item">
     <label>
         <input type="checkbox" bind:checked={comprado}/>
         {descricao}
     </label>
     <button on:click={() => dispatch("itemremovido")}>Excluir</button>
 </div>
+
+<style>
+    .container-item{
+        display: flex;
+        justify-content: space-between;
+        padding: 1rem ;
+        align-items: center;
+        background-color:#5b7c5b;
+        border-bottom: 1px solid hsl(0, 0%, 100%);
+    }
+    label{
+        font-size: 1.2rem;
+    }
+    button{
+        padding: 0.5rem;
+        width: 5rem;
+        background-color: hsl(16, 100%, 66%);
+        color: hsl(0, 0%, 100%);
+        border: none;
+    }
+    button:hover{
+        background-color: hsl(16, 100%, 78%);
+        cursor: pointer;
+    }
+    
+</style>
